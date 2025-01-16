8b862ea95d50128fcfa63cb6932d0c9380b9a3f6
Arshiya Hashemzadeh <last2fsyrs@gmail.com>
2025-01-17 03:11:01 +0330

    top syscall done!

--- user/top.c
+++ user/top.c
@@ -6,41 +6,41 @@
 //#include "kernel/defs.h"
 #include "user.h"
 
-//static char *states[] = {
-//        [UNUSED]    "unused",
-//        [USED]      "used",
-//        [SLEEPING]  "sleep ",
-//        [RUNNABLE]  "runble",
-//        [RUNNING]   "run   ",
-//        [ZOMBIE]    "zombie"
-//};
+static char *states[] = {
+        [UNUSED]    "unused",
+        [USED]      "used",
+        [SLEEPING]  "sleep ",
+        [RUNNABLE]  "runble",
+        [RUNNING]   "run   ",
+        [ZOMBIE]    "zombie"
+};
 
 int main() {
-    struct top *top_struct = (struct top  *) malloc(sizeof (struct top *));
+    struct top top_struct;
 
     int id = fork();
     if (id == 0) {
 
-        for (int i = 0; i < 10000; ++i) {
+        for (int i = 0; i < 1000000000; ++i) {
 
         }
-        top_func(top_struct);
-//        printf("%d", top_struct->count);
+        top_func(&top_struct);
 
     } else {
-        for (int i = 0; i < 10; ++i) {
+        for (int i = 0; i < 900000000; ++i) {
+
+        }
+        for (int i = 0; i < 900000000; ++i) {
 
         }
 
-        sleep(10);
+        sleep(20);
+        exit(0);
     }
-
-
-//    printf("from user!\n");
-//    printf("number of process:%d\nPID\t\tPPID\t\tSTATE\t\tNAME\t\tUSAGE\\t\n",top_struct->count);
-//    for (int j = 0; j < top_struct->count; ++j) {
-//        printf("%d\t\t%d\t\t%s\t\t%s\t\t%d\n", top_struct->processes[j].pid,top_struct->processes[j].ppid,top_struct->processes[j].pid,states[top_struct->processes[j].state],top_struct->processes[j].name,top_struct->processes[j].usage.sum_of_ticks);
-//    }
-
-
+    int n = top_struct.count;
+    printf("number of process:%d\nPID\t\tPPID\t\tSTATE\t\tNAME\t\tUSAGE\n",n);
+    for (int j = n - 1; j >= 0 ; --j) {
+        printf("%d\t\t%d\t\t%s\t\t%s\t\t%d\n", top_struct.processes[j].pid,top_struct.processes[j].ppid,states[top_struct.processes[j].state],top_struct.processes[j].name,top_struct.processes[j].usage.sum_of_ticks);
+    }
+    sleep(20);
 }
